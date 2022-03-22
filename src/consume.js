bae87108996ac5d93eaad7d286bbe157686892b9
Michael Pistrang <michael.pistrang@vetrofibermap.com>
2022-03-21 20:06:38 -0400

    break out message processing and wrap with span context init

--- src/consume.js
+++ src/consume.js
@@ -1,10 +1,30 @@
 require("./tracing.js");
 
 const api = require("@opentelemetry/api");
-const axios = require('axios');
+const axios = require("axios");
 const { kafka } = require("./kafka.js");
 
 const tracer = api.trace.getTracer("message-consumer");
+
+const processMessage = async ({ topic, partition, message }) => {
+  // get any web page
+  await axios.get(
+    "https://docs.honeycomb.io/getting-data-in/opentelemetry/javascript/"
+  );
+  const { value, timestamp } = message;
+
+  console.log({
+    value: value.toString(),
+    timestamp: timestamp.toString(),
+  });
+};
+
+const processMessageWrapper = async (args) => {
+  const span = tracer.startSpan("message received");
+  const ctx = api.trace.setSpan(api.context.active(), span);
+  await api.context.with(ctx, processMessage, undefined, args);
+  span.end();
+};
 
 const consume = async () => {
   const consumer = kafka.consumer({ groupId: "test-group" });
@@ -13,18 +33,7 @@ const consume = async () => {
   await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
 
   await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      const span = tracer.startSpan("consume message");        
-      // get any web page 
-      await axios.get('https://docs.honeycomb.io/getting-data-in/opentelemetry/javascript/')
-      const {value, timestamp} = message
-
-      console.log({
-        value: value.toString(),
-        timestamp: timestamp.toString(),
-      });      
-      span.end()
-    },
+    eachMessage: processMessageWrapper,
   });
 };
 
